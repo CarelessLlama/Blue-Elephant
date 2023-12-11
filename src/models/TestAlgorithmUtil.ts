621a7672688264a52408acab744654c496d55c05
Nicholas <nicholaschiazhijie2001@yahoo.com>
2023-12-12 00:44:06 +0800

    Implement algorithm
    Graph partitioning into n groups with size as equal as possible is likely a NP-hard problem. Hence, we turn to an algorithm to look for an optimal solution.
    1. Sort edges by decreasing weight
    2. Add edges to a graph until a complete graph of size n is obtained
    3. Make these nodes of the graph into a set each
    4. Add the rest of the nodes by minimising total weight

---
+++ src/models/TestAlgorithmUtil.ts
@@ -0,0 +1,45 @@
+import { Project } from './Project';
+
+/**
+ * Utility class for testing the algorithm
+ */
+class AlgorithmTester {
+    public constructor() {}
+
+    /**
+     * Generate a symmetrical matrix
+     * @param size Size of the matrix
+     * @returns a symmetrical matrix
+     */
+    public static generateSymmetricalMatrix(size: number): number[][] {
+        let matrix = Array(size)
+            .fill(null)
+            .map(() => Array(size).fill(0));
+        for (let i = 0; i < size; i++) {
+            for (let j = i; j < size; j++) {
+                let randomValue = Math.floor(Math.random() * 5) + 1;
+                matrix[i][j] = randomValue;
+                matrix[j][i] = randomValue;
+            }
+        }
+        return matrix;
+    }
+
+    /**
+     * Generate a project
+     * @param n Size of the project
+     * @returns a project
+     */
+    public static generateProject(n: number): Project {
+        return new Project(
+            1,
+            1,
+            'test',
+            'test',
+            this.generateSymmetricalMatrix(n),
+            ['test1', 'test2'],
+        );
+    }
+}
+
+export { AlgorithmTester };
---
+++ src/models/sampleTest.ts
@@ -0,0 +1,7 @@
+import { AlgorithmRunner } from './AlgorithmRunner';
+import { AlgorithmTester } from './TestAlgorithmUtil';
+
+const project = AlgorithmTester.generateProject(9);
+console.log(project.getAdjMatrix());
+const groupings = AlgorithmRunner.run(project, 3);
+// console.log(groupings);
