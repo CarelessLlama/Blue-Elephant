a4db12d03afad742d1757d9499195ff1152048b1
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-12-13 00:44:40 +0800

    Attempt to fix codecov reading wrong file

---
+++ test/models/Person.test.ts
@@ -0,0 +1,8 @@
+import { Person } from '../../src/models/Person';
+
+describe('Person', () => {
+    it('should initialize correctly', () => {
+        const person = new Person('name', 1);
+        expect(person.getId()).toEqual(1);
+    });
+});
