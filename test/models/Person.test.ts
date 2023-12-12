8cee41c394ef4b2b576dd78dee645ac5558ba1fa
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-12-13 01:26:14 +0800

    Fix bug in Person.test.ts

--- test/models/Person.test.ts
+++ test/models/Person.test.ts
@@ -4,6 +4,6 @@ describe('Person', () => {
     it('should initialize correctly', () => {
         const person = new Person('name', 1);
         expect(person.getId()).toEqual(1);
-        expect(person.toString).toEqual('name');
+        expect(person.toString()).toEqual('name');
     });
 });
