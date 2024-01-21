aa103c5160c8fcbc126994a42c15f1234c19d739
Nicholas Chia <e0957944@u.nus.edu>
2024-01-21 22:02:36 +0800

    Fix linter issues

--- src/models/sampleTest.ts
+++ src/models/sampleTest.ts
@@ -1,9 +1,7 @@
 import { AlgorithmRunner } from './AlgorithmRunner';
 import { AlgorithmTester } from './TestAlgorithmUtil';
-import { Project } from './Project';
 
 // Random project with 27 nodes and 10 groups and random weights
 const project = AlgorithmTester.generateProjectWithRandomWeights(27);
 const logic = new AlgorithmRunner(project, 10); // 3 is the number of groups desired
 console.log(logic.prettyPrintGroupings());
-
